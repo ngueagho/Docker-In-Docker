9f07b68fcde83a8e109611bbef1063f3097fe665
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2024-07-06 18:59:16 +0100

    06-072024

--- docker_install.sh
+++ docker_install.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-sudo apt update 
+ sudo apt update 
 
-sudo apt upgrade -y
+ sudo apt upgrade -y
 
 # apt upgrade -y
 
-sudo apt install curl -y
+# sudo apt install curl -y
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
+# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
 
-sudo apt update -y
+# sudo apt update -y
 
-apt-cache policy docker-ce -y
+# apt-cache policy docker-ce -y
 
-sudo apt install docker-ce -y
+# sudo apt install docker-ce -y
 
-sudo systemctl status docker
+# sudo systemctl status docker
+
+ sudo apt install docker.io -y
+
